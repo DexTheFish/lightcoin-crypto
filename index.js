440ed69cc5ab2599c2e37952a2d1d4f5f25e4a50
Declan Davis <dexthefish@gmail.com>
2022-06-10 19:38:26 -0400

    added classes Account and Transaction. Added subclasses Deposit and Withdrawal of Transaction. Added driver code to test these new classes with a simple example.

--- index.js
+++ index.js
@@ -1,29 +1,61 @@
-let balance = 500.00;
+class Account {
 
-class Withdrawal {
-
-  constructor(amount) {
-    this.amount = amount;
-  }
-
-  commit() {
-    balance -= this.amount;
+  constructor(username) {
+    this.username = username;
+    // Have the account balance start at $0 since that makes more sense.
+    this.balance = 0;
   }
 
 }
 
 
 
+class Transaction {
+
+  constructor(amount, account) {
+    this.amount  = amount;
+    this.account = account;
+  }
+
+}
+
+class Deposit extends Transaction {
+
+  commit() {
+    this.account.balance += this.amount;
+  }
+
+}
+
+class Withdrawal extends Transaction {
+
+  commit() {
+    this.account.balance -= this.amount;
+  }
+
+}
+
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account("green-day");
+myAccount.balance = 500;
+
+
+console.log("--------------WITHDRAWAL-------------------")
+t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
 console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+console.log('myAccount Balance:', myAccount.balance);
+console.log("-----------------------------------\n");
+console.log("\n---------------DEPOSIT-------------------")
+t3 = new Deposit(120.00, myAccount);
+t3.commit();
+console.log('Transaction 3:', t3);
+console.log("-----------------------------------\n");
