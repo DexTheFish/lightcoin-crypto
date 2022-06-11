863fd42c7cbffa9c4dc19fcdd9fb298f968413c3
Declan Davis <dexthefish@gmail.com>
2022-06-10 20:20:11 -0400

    changed Deposit and Withdrawal to use getters; moved the commit() login into superclass Transaction.

--- index.js
+++ index.js
@@ -17,21 +17,28 @@ class Transaction {
     this.account = account;
   }
 
+  commit() {
+    this.account.balance += this.value;
+  }
+  // cant alter .value
+
 }
 
 class Deposit extends Transaction {
 
-  commit() {
-    this.account.balance += this.amount;
+  get value() {
+    return this.amount;
   }
 
 }
 
 class Withdrawal extends Transaction {
 
-  commit() {
-    this.account.balance -= this.amount;
+  get value() {
+    return -this.amount;
   }
+
+
 
 }
 
@@ -40,22 +47,16 @@ class Withdrawal extends Transaction {
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
 const myAccount = new Account("green-day");
-myAccount.balance = 500;
 
+console.log('Starting Balance:', myAccount.balance);
 
-console.log("--------------WITHDRAWAL-------------------")
-t1 = new Withdrawal(50.25, myAccount);
+const t1 = new Deposit(120.00, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99, myAccount);
+const t2 = new Withdrawal(50.00, myAccount);
+
+t2.value = 1000000000 //not allowed
+
 t2.commit();
-console.log('Transaction 2:', t2);
 
-console.log('myAccount Balance:', myAccount.balance);
-console.log("-----------------------------------\n");
-console.log("\n---------------DEPOSIT-------------------")
-t3 = new Deposit(120.00, myAccount);
-t3.commit();
-console.log('Transaction 3:', t3);
-console.log("-----------------------------------\n");
+console.log('Ending Balance:', myAccount.balance);
